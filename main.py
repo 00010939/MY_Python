af6bc09ddab735b5e76d19a9affe89b4204c5475
00011818 <wiut11818@gmail.com>
2021-10-28 14:05:03 +0500

    132

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("Hello world")
 print("Hello world")
+# sal;d,sl;d,lsks
+
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
 print("Hello world")
